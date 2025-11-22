5a9d74fae0cefca7628a5cec2201da2f828efcc3
Flame787 <mbrezovic77@gmail.com>
2025-11-22 23:42:11 +0100

    added quantity and buy-button to ItemCard

--- frontend/src/components/Product/ItemCard.jsx
+++ frontend/src/components/Product/ItemCard.jsx
@@ -5,6 +5,8 @@
 // import ItemPrice from "./ItemPrice";
 // import ItemQuantity from "./ItemQuantity";
 
+import { useState } from "react";
+
 export default function ItemCard({
   name,
   description,
@@ -16,7 +18,12 @@ export default function ItemCard({
   tags,
   className,
 }) {
-  // turn into number (if null or undefined, fallback to 0)
+  // State for quantity-button:
+  const [qty, setQty] = useState(1);
+  const increase = () => setQty((q) => q + 1);
+  const decrease = () => setQty((q) => (q > 1 ? q - 1 : 1));
+
+  // turn price into number (if null or undefined, fallback to 0)
   const numericPrice = price ? Number(price) : 0;
   const numericDiscount = discountPrice ? Number(discountPrice) : null;
 
@@ -59,6 +66,26 @@ export default function ItemCard({
             ))}
           </div>
         )}
+
+        <div className="item-bottom">
+          <div className="quantity-selector">
+            <label>Quantity:</label>
+
+            <div className="quantity-box">
+              <button onClick={decrease} className="qty-btn">
+                −
+              </button>
+
+              <span className="qty-value">{qty}</span>
+
+              <button onClick={increase} className="qty-btn">
+                +
+              </button>
+            </div>
+          </div>
+
+          <button className="link buy-button">Add to Cart</button>
+        </div>
 
         {/* <p className="item-category">Category: {category}</p> */}
       </div>
